ab3e8ee4f6a926527991b6bf222e8bf5822cfa78
zye <yezhenyu.sam@gmail.com>
2013-03-13 18:04:03 +0100

    change cmake files

--- kernels/gaussian5x5int/run.sh
+++ kernels/gaussian5x5int/run.sh
@@ -1,1 +1,3 @@
-../../
+../../bin/gaussian5x5int_dmem_init ./dmem/lenna.pgm ./dmem/gaussian5x5int.dmem_init ./dmem/gaussian5x5int.dmem_meta
+../../bin/gaussian5x5int ./dmem/gaussian5x5int.dmem_init ./dmem/gaussian5x5int.dmem_meta ./dmem/gaussian5x5int.result
+../../bin/gaussian5x5int_dmem_dump ./dmem/gaussian5x5int.result ./dmem/gaussian5x5int.dmem_meta ./dmem/out.pgm
